9a8d137c2ea1a06228bf3c7261487f296b78b944
Codidodido <codidodido@gmail.com>
2023-09-14 21:42:30 +0330

    Implement hacking type race game with selenium

--- hack.py
+++ hack.py
@@ -1,4 +1,25 @@
 from selenium import webdriver
+from time import sleep
 
 driver = webdriver.Firefox()
 driver.get(f"https://play.typeracer.com")
+sleep(10)
+
+enter = driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div/div[1]/div[2]/table/tbody/tr[2]/td[2]/div/div[1]/div/div[1]/div[2]/a")
+enter.click()
+
+sleep(13)
+sentence = driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div/div[1]/div[2]/table/tbody/tr[2]/td[2]/div/div[1]/div/table/tbody/tr[2]/td[3]/table/tbody/tr[2]/td/table/tbody/tr[1]/td/table/tbody/tr[1]/td/div/div")
+matn = sentence.text
+print(matn)
+
+
+matn = matn.split()
+
+entry = driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div/div[1]/div[2]/table/tbody/tr[2]/td[2]/div/div[1]/div/table/tbody/tr[2]/td[3]/table/tbody/tr[2]/td/table/tbody/tr[2]/td/input")
+
+for word in matn:
+    for alph in word:
+        entry.send_keys(alph)
+        sleep(0.1)
+    entry.send_keys(" ")
